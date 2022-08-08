8506413225b52e554c4a041558a0139fdf3db5f7
siya <siya9556821429@gmail.com>
2022-08-08 19:40:15 +0530

    changed donation urls

--- donations/urls.py
+++ donations/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('previous_donations_list/', views.previous_donations_list, name='previous_donations_list'),
-    path('new_donation', views.new_donation, name='new_donation'),
-    path('donation_payment_id', views.donation_payment_id, name='donation_payment_id'),
-    path('donation_detail', views.donation_detail, name='donation_detail')
+    path('', views.list, name='donation_list'),
+    path('new/', views.new, name='donation_new'),
+    path('donation_payment_id/', views.donation_payment_id, name='donation_payment_id'),
+    path('<int:id>/detail/', views.detail, name='donation_detail')
 ]
