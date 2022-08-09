e67d7234337c916cdf1811ef10e2256439de3aad
siya <siya9556821429@gmail.com>
2022-08-09 19:10:23 +0530

    modified event urls

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.list, name='event_list'),
     path('<int:id>/detail/', views.detail, name='event_detail'),
-    path('new/', views.new, name='event_request_new'),
+    path('new/', views.new, name='event_new'),
 
 ]
