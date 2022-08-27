7cdb4fd4552fd0a5fbed5db2c6b13dbe6da769df
siya <siya9556821429@gmail.com>
2022-08-27 11:43:53 +0530

    contact us model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField()
+  email = models.EmailField(max_length=100)
+  phone = models.CharField(max_length=15, blank=True, null=True)
+
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    db_table = 'contacts'
+
+  def __str__(self):
+    return self.name
--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
+from django.contrib import messages
 from django.shortcuts import render
-
+from .models import Contact
 # Create your views here.
 def index(request):
   return render(request, 'home/index.html')
 
 def contact_us(request):
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    description = request.POST.get('description')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    contact = Contact(name=name, description=description, email=email, phone=phone)
+    contact.save()
+    messages.success(request, 'We have recieved your request.')
   return render(request, 'home/contact_us.html')
 
 
